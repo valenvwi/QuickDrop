27eb5cabea67e77613cdccfe242a6822553911a1
Wai Ian Vong <valenvwi@gmail.com>
2023-06-06 10:49:15 +0200

    validations for addresses

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :driver, optional: true
+
+  validates :pickup_address, presence: true
+  validates :pickup_at, presence: true
+  validates :dropoff_address, presence: true
+
+  validate :check_address
+
+  def check_address
+    errors.add(:dropoff_address, "can't be the same as pick up address") if pickup_address == dropoff_address
+  end
 end
