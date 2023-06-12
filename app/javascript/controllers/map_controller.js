11718a35416d9b9686d4b6d3fdaf4c36b44dc55c
micaela frigola <mica_frigola@icloud.com>
2023-06-12 15:19:54 +0200

    map zoom change from 11 to 9

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10",
       center: coords,
-      zoom: 11
+      zoom: 9
     })
 
     this.#addMarkersToMap()
